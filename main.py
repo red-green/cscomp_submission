2a72614679f8e972c0bce6851715ca288c286d3d
Jackson Servheen <jservheen@gmail.com>
2016-03-17 19:27:29 -0600

    added resubmission and architects (authors)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if not os.path.exists(DATABASE):
 	print "creating new db..."
 	conn = sqlite3.connect(DATABASE)
 	c = conn.cursor()
-	c.execute('CREATE TABLE programs (hash text, team text, problem text, code text, ts int)')
+	c.execute('CREATE TABLE programs (hash text, team text, problem text, code text, ts int, resubmit boolean, architects text)')
 	conn.commit()
 	conn.close()
 
@@ -24,9 +24,11 @@ def submit():
 	team = request.form.get('team_name','_error_')
 	problem = request.form.get('problem_name','_error_')
 	program = request.form.get('code','_error_')
+	resub = request.form.get('resubmit','_error_') == 'on'
+	arch = request.form.get('architects','_error_')
 	conn = sqlite3.connect(DATABASE)
 	c = conn.cursor()
-	c.execute('INSERT INTO programs VALUES (?,?,?,?,?)',(hashlib.md5(program).hexdigest(),team,problem,program,time.time()))
+	c.execute('INSERT INTO programs VALUES (?,?,?,?,?,?,?)',(hashlib.md5(team+problem+program).hexdigest(),team,problem,program,time.time(),resub,arch))
 	conn.commit()
 	conn.close()
 	return redirect('/?t={}'.format(urllib.quote(team)),302)
@@ -38,7 +40,7 @@ def view(cs):
 	c.execute('SELECT * FROM programs WHERE hash=?',(cs,))
 	i = c.fetchone()
 	conn.close()
-	a = {'hash':i[0],'team':i[1],'problem':i[2],'time':time.strftime('%I:%M:%S %p',time.gmtime(i[4])),'code':i[3]}
+	a = {'hash':i[0],'team':i[1],'problem':i[2],'time':time.strftime('%I:%M:%S %p',time.gmtime(i[4])),'code':i[3],'resub':i[5],'arch':i[6]}
 	return render_template('view.html',row=a)
 
 @app.route('/view/'+SECRET)
@@ -48,7 +50,7 @@ def submissions():
 	c.execute('SELECT * FROM programs ORDER BY ts DESC') # most recent first?
 	table = []
 	for i in c.fetchall():
-		table.append({'hash':i[0],'team':i[1],'problem':i[2],'time':time.strftime('%I:%M:%S %p',time.gmtime(i[4]))})
+		table.append({'hash':i[0],'team':i[1],'problem':i[2],'time':time.strftime('%I:%M:%S %p',time.gmtime(i[4])),'resub':i[5],'arch':i[6]})
 	conn.close()
 	return render_template('submissions.html',table=table,root='/view/'+SECRET+'/')
 
